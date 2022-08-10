573613e928a259331af960fa052292f1f24f3cd6
Gabriel Ruiz Varela <gabrielruizvarela@gmail.com>
2022-08-10 10:35:13 -0300

    Added Profile

--- src/components/GeneralInformation.js
+++ src/components/GeneralInformation.js
@@ -1,16 +1,23 @@
 import React from "react";
 // import "./style/GeneralInformation.css";
 import Field from "./Field";
-// import { nanoid } from "nanoid";
+import { nanoid } from "nanoid";
 
 class GeneralInformation extends React.Component {
   render() {
+    const { generalInformation, skills } = this.props.init;
     return (
-        <Field
-          className={this.props.className}
-          defaultValues={this.props.defaultValues}
-        />
-        );
+      <div className="GeneralInformation">
+        {
+          Object.keys(generalInformation).map((name) => {
+            return <Field defaultValues={generalInformation[name]} className={name} key={nanoid()} />
+          })
+        }
+        {<h1 className="title">Skills</h1>}
+        {<Field defaultValues={skills} className="skills" key={nanoid()} />}
+
+      </div>
+    );
   }
 }
 
